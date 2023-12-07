7b4cfe7fefdc52101e459440ff179e88c52304b5
dagopia <tdaguti@gmail.com>
2023-12-07 12:06:53 +0300

    style Updated.

---
+++ main.js
@@ -0,0 +1,21 @@
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+
+    // Simple validation, you can add more complex validation if needed
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+      alert("All fields must be filled out");
+      return false;
+    }
+
+    // You can add more specific validation for the email field
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address");
+      return false;
+    }
+
+    // If the validation passes, the form will be submitted
+    return true;
+  }
