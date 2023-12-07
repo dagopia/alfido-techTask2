f5cdbb14330126276d8af343331e4195e9d07766
dagopia <tdaguti@gmail.com>
2023-12-07 12:14:22 +0300

    updated.

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 function validateForm() {
-    var name = document.forms["contactForm"]["name"].value;
-    var email = document.forms["contactForm"]["email"].value;
-    var message = document.forms["contactForm"]["message"].value;
+  var name = document.forms["contactForm"]["name"].value;
+  var email = document.forms["contactForm"]["email"].value;
+  var message = document.forms["contactForm"]["message"].value;
 
-    // Simple validation, you can add more complex validation if needed
-    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
-      alert("All fields must be filled out");
-      return false;
-    }
-
-    // You can add more specific validation for the email field
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      alert("Please enter a valid email address");
-      return false;
-    }
-
-    // If the validation passes, the form will be submitted
-    return true;
+  // Simple validation, you can add more complex validation if needed
+  if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+    alert("All fields must be filled out");
+    return false;
   }
+
+  // You can add more specific validation for the email field
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+
+  showMessage();
+  return true;
+}
+function showMessage() {
+  alert("Thank you for submitting your message!");
+}
